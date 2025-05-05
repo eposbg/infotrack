0b935e9594065595e2b9e31ad23daa2236f9e789
Emanuil Stoyanov <emanuil.stoyanov@gmail.com>
2025-05-05 12:20:20 +0100

    skyfold search structure in the api

---
+++ code/server/src/RatingTracker.Application/Services/SearchService.cs
@@ -0,0 +1,16 @@
+﻿using RatingTracker.Domain.SearchEngines;
+using RatingTracker.Infrastructure.SearchEngineCrawler;
+
+namespace RatingTracker.Application.Services;
+
+public class SearchService(ISearchEngineCrawlerFactory searchEngineCrawlerFactory) : ISearchService
+{
+    public async Task<int> SearchAsync(string keywords, string targetDomain, int maxResults,
+        CancellationToken token = default)
+    {
+        var crawler = searchEngineCrawlerFactory.Create(SearchEngineType.Google);
+        var result  = await crawler.GetSearchRanksAsync(keywords, targetDomain, maxResults, token);
+
+        return 0;
+    }
+}
