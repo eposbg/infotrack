7d6b55eafc55f2334073d38d4d1333b8a423358e
Emanuil Stoyanov <emanuil.stoyanov@gmail.com>
2025-05-06 08:42:10 +0100

    Google scraper service tests

--- code/server/tests/RatingTracker.UnitTests/Infrastructure/ScraperServices/GoogleScraperServiceTests.cs
+++ code/server/tests/RatingTracker.UnitTests/Infrastructure/ScraperServices/GoogleScraperServiceTests.cs
@@ -7,17 +7,50 @@ using RatingTracker.Infrastructure.ScraperServices;
 
 namespace RatingTracker.UnitTests.Infrastructure.ScraperServices;
 
-public class BingScraperServiceTests
-{
-    [Fact]
+
+public class GoogleScraperServiceTests
+ {
+   [Fact]
     public async Task GetSearchRanksAsync_ShouldReturnCorrectRank_WhenTargetUrlIsPresent()
     {
         // Arrange
         var html = """
-                       <li class="b_algo">
-                         <h2><a href="https://www.infotrack.co.uk/some-path">Some Result</a></h2>
-                       </li>
-                   """;
+            <a href="/url?q=https://www.infotrack.co.uk/some-path&amp;">
+        """;
+
+        var handlerMock = new Mock<HttpMessageHandler>();
+        handlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.Is<HttpRequestMessage>(msg =>
+                    msg.RequestUri!.ToString().Contains("google.co.uk")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(html),
+            });
+
+        var httpClient = new HttpClient(handlerMock.Object);
+        var logger = Mock.Of<ILogger<GoogleScraperService>>();
+        var service = new GoogleScraperService(httpClient, logger);
+
+        // Act
+        var result = await service.GetSearchRanksAsync("land registry searches", "infotrack.co.uk");
+
+        // Assert
+        result.SearchEngine.Should().Be("Google");
+        result.Ranks.Should().Contain(1);
+    }
+
+    [Fact]
+    public async Task GetSearchRanksAsync_ShouldReturnZero_WhenTargetUrlNotPresent()
+    {
+        // Arrange
+        var html = """
+            <a href="/url?q=https://someotherdomain.com/page&amp;">
+        """;
 
         var handlerMock = new Mock<HttpMessageHandler>();
         handlerMock
@@ -33,75 +66,47 @@ public class BingScraperServiceTests
             });
 
         var httpClient = new HttpClient(handlerMock.Object);
-        var logger = Mock.Of<ILogger<BingScraperService>>();
-        var service = new BingScraperService(httpClient, logger);
+        var logger = Mock.Of<ILogger<GoogleScraperService>>();
+        var service = new GoogleScraperService(httpClient, logger);
 
         // Act
         var result = await service.GetSearchRanksAsync("land registry searches", "infotrack.co.uk");
 
         // Assert
-        result.SearchEngine.Should().Be("Bing");
-        result.Ranks.Should().Contain(1); // First result
+        result.Ranks.Should().BeEmpty();
     }
 
     [Fact]
-    public async Task GetSearchRanksAsync_ShouldReturnZero_WhenTargetUrlNotPresent()
+    public async Task GetSearchRanksAsync_ShouldLogError_OnHttpFailure()
     {
         // Arrange
-        var html = """
-                       <li class="b_algo">
-                         <h2><a href="https://someotherdomain.com/page">Unrelated Result</a></h2>
-                       </li>
-                   """;
-
         var handlerMock = new Mock<HttpMessageHandler>();
         handlerMock
             .Protected()
-            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>())
             .ReturnsAsync(new HttpResponseMessage
             {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(html),
+                StatusCode = HttpStatusCode.Forbidden
             });
 
         var httpClient = new HttpClient(handlerMock.Object);
-        var logger = Mock.Of<ILogger<BingScraperService>>();
-        var service = new BingScraperService(httpClient, logger);
+        var loggerMock = new Mock<ILogger<GoogleScraperService>>();
+        var service = new GoogleScraperService(httpClient, loggerMock.Object);
 
         // Act
         var result = await service.GetSearchRanksAsync("land registry searches", "infotrack.co.uk");
 
         // Assert
         result.Ranks.Should().BeEmpty();
-    }
 
-    [Fact]
-    public async Task GetSearchRanksAsync_ShouldLogError_OnException()
-    {
-        // Arrange
-        var handlerMock = new Mock<HttpMessageHandler>();
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>())
-            .ThrowsAsync(new HttpRequestException("Bing is down"));
-
-        var httpClient = new HttpClient(handlerMock.Object);
-        var loggerMock = new Mock<ILogger<BingScraperService>>();
-
-        var service = new BingScraperService(httpClient, loggerMock.Object);
-
-        // Act
-        var result = await service.GetSearchRanksAsync("land registry searches", "infotrack.co.uk");
-
-        // Assert
-        result.Ranks.Should().BeEmpty();
         loggerMock.Verify(
             l => l.Log(
                 LogLevel.Error,
                 It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains("Bing is down")),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains("Failed to fetch")),
                 It.IsAny<Exception>(),
                 It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
             Times.Once);
