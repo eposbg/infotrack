d41151f399fdbb26c08e52a0328aca3bf297fdb6
Emanuil Stoyanov <emanuil.stoyanov@gmail.com>
2025-05-07 07:33:01 +0100

    Keyword cleanup function is improved

--- code/server/src/RatingTracker.Application/Helpers/KeywordHelper.cs
+++ code/server/src/RatingTracker.Application/Helpers/KeywordHelper.cs
@@ -4,8 +4,7 @@ public static class KeywordHelper
 {
     public static string KeywordsCleanup(string rawKeywords)
     {
-        return string.Join(",", rawKeywords
-            .Split(',', StringSplitOptions.RemoveEmptyEntries)
-            .Select(k => k.Trim().Replace(" ", "")));
+        return string.Join(" ", rawKeywords
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries));
     }
 }
